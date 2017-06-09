a871b2572a8cc8fd92d7640d5a18abf74b0edfe2
mpajkowski <marcin.pajkowski@gmail.com>
2017-06-09 17:55:28 +0200

    gotowe

--- uruchom.sh
+++ uruchom.sh
@@ -30,9 +30,9 @@ rozmyslanie()
 ##
 konsumpcja()
 { 
-	wiadomosc $1 "Jem posilek $2, zajmie mi to $3 s\n"
+	wiadomosc $1 "Kosnumuje posilek $2, zajmie mi to $3 s\n"
 	sleep $3
-	wiadomosc $1 "Juz zjadlem posilek nr $2\n"
+	wiadomosc $1 "Posilek nr $2 zjedzony \n"
 }
 
 ##################################################################################
@@ -42,7 +42,7 @@ konsumpcja()
 ##
 czyPosprzatac()
 {
-	wiadomosc "(:x:)" "Czy posprzatac po filozofach? (t/T) "
+	wiadomosc "(:x:)" "Czy posprzatac po uczcie? (t/T) "
 	read -n 1 wybor
 	echo ""
 	if [[ $wybor == "t" ]] || [[ $wybor == "T" ]]; then
@@ -52,9 +52,9 @@ czyPosprzatac()
 		done
 		rm -f $1
 		rm -rf $2
-		wiadomosc ":x:" "Usunieto!\n"
+		wiadomosc ":)" "Usunieto!\n"
 	else
-		wiadomosc ":x:" "Nie sprzatam.\n"
+		wiadomosc ":)" "Nie sprzatam.\n"
 	fi
 }
 
@@ -90,7 +90,6 @@ czyRzeczywista()
 ##
 filozof()
 {
-	
 	#wiadomosc START W ZALEZNOSCI OD ILOSCI ARGUMENTOW
 	if [ $# -eq  8 ]; then
 		#wiadomosc STARTU
@@ -102,8 +101,8 @@ filozof()
 		wiadomosc $1 "^^^START^^^ Liczba posilkow:$2 | Liczba filozofow:$3 | Sciezka katalogu:$4 | Plik bariery:$5 |
 		| Plik potoku: $8 | Bez. sciezka do pierwszego widelca (WID:$6): $(realpath $4/$6) | Drugi widelec WID:$7 | Czas konsumowania:$9 s | Czas rozmyslania:${10} s\n"
 		local readonly PODANO_CZASY=1
-		local declare czas_konsumpcji=$9
-		local declare czas_rozmyslania=$10
+		local declare czasKonsumpcji=$9
+		local declare czasRozmyslania=${10}
 	fi
 
 	local readonly FID=$1
@@ -118,50 +117,47 @@ filozof()
 	declare -i skonsumowaneLiczba=0;
 	(( polowa_posilkow = posilkiLiczba-posilkiLiczba/2 ))
 
-	#ZADEKLAROWANIE DESKRYPTOROW (NIE MOZNA UZYWAC LITERALOW)
-	local pierwszy_deskryptor=9
-	local drugi_deskryptor=10
+	#ZADEKLAROWANIE DESKRYPTOROW
+	local pierwszyDeskryptor=9
+	local drugiDeskryptor=10
 
 	#PRZYPISANIE DESKRYPTORÓW
-	eval exec "${pierwszy_deskryptor}>${sciezkaKatalogu}/${pierwszyWidelec}"
-	eval exec "${drugi_deskryptor}>${sciezkaKatalogu}/${drugiWidelec}"
+	eval exec "${pierwszyDeskryptor}>${sciezkaKatalogu}/${pierwszyWidelec}"
+	eval exec "${drugiDeskryptor}>${sciezkaKatalogu}/${drugiWidelec}"
 	
 	# "CYKL" FILOZOFA
 	while [ true ]; do
 
-				
-		#LOSOWANIE CZASOW_ROZMYSLANIA I KONSUMPCJI JESLI NIE BYLY PODANE
+		#LOSOWANIE CZASOW: ROZMYSLANIA, KONSUMPCJI
 		if [ $# -eq 8 ] ; then 
-			local declare czas_konsumpcji="0.$RANDOM"
-			local declare czas_rozmyslania="0.$RANDOM"
-		fi;
-		
+			local declare czasKonsumpcji="0.$RANDOM"
+			local declare czasRozmyslania="0.$RANDOM"
+		fi
 		#PROBA PODNIESIENIA PIERWSZEGO WIDELCA
 		wiadomosc $FID "Probuje podniesc widelec (WID:$pierwszyWidelec)...\n"
-		flock -e $pierwszy_deskryptor
+		flock -e $pierwszyDeskryptor
 		wiadomosc $FID "Podnioslem widelec (WID:$pierwszyWidelec) i zalozylem na nim blokade wylaczna\n"
 
 		#PROBA PODNIESIENIA DRUGIEGO WIDELCA
 		wiadomosc $FID "Probuje podniesc widelec (WID:$drugiWidelec)...\n"
-		flock -e $drugi_deskryptor
+		flock -e $drugiDeskryptor
 		wiadomosc $FID "Podnioslem widelec (WID:$drugiWidelec) i zalozylem na nim blokade wylaczna\n"
 
 		#KONSUMOWANIE
 		nastepnyPosilek=$[skonsumowaneLiczba+1]
-		konsumpcja $FID $nastepnyPosilek $czas_konsumpcji
+		konsumpcja $FID $nastepnyPosilek $czasKonsumpcji
 		skonsumowaneLiczba=nastepnyPosilek
 
 		#ODKLADANIE WIDELCOW W ODWROTNEJ KOLEJNOSCI	
-		flock -u $drugi_deskryptor
+		flock -u $drugiDeskryptor
 		wiadomosc $FID "Odlozylem widelec (WID:$drugiWidelec) i zdjalem z niego blokade wylaczna\n" 
-		flock -u $pierwszy_deskryptor
+		flock -u $pierwszyDeskryptor
 		wiadomosc $FID "Odlozylem widelec (WID:$pierwszyWidelec) i zdjalem z niego blokade wylaczna\n" 
 		
 		#wiadomosc WYSWIETLANY PO ZJEDZENIU POLOWY ZE WSZYSTKICH POSILKOW
 		if [[ $skonsumowaneLiczba == $polowa_posilkow ]]; then
 			wiadomosc $FID "---POLOWA--- Zjadlem juz $skonsumowaneLiczba posilkow(-ki)\n"
 			coproc ncat -U  $sciezkaBariery -c 'read'   
-			#fuser -k $sciezkaBariery 
 			cat $sciezkaPotoku  >> /dev/null  # zeby oczekiwac na barierze
 		fi
 		
@@ -172,7 +168,7 @@ filozof()
 		fi;
 			
 		#ROZMYSLANIE
-		rozmyslanie $FID $skonsumowaneLiczba $czas_rozmyslania   
+		rozmyslanie $FID $skonsumowaneLiczba $czasRozmyslania   
 		
 	done
 
@@ -185,111 +181,121 @@ filozof()
 while getopts f:n:s:r:k: OPCJA
 do
     case $OPCJA in
-        f) declare -i LICZBA_FILOZOFOW=$OPTARG;;
-        n) declare -i LICZBA_POSILKOW=$OPTARG;;
-        s) declare SCIEZKA_KATALOGU=$OPTARG;;       
-        r) declare CZAS_ROZMYSLANIA=$OPTARG;;
-        k) declare CZAS_KONSUMPCJI=$OPTARG;;
+        f) declare -i liczbaFilozofow=$OPTARG;;
+        n) declare -i liczbaPosilkow=$OPTARG;;
+        s) declare sciezkaKatalogu=$OPTARG;;       
+        r) declare czasRozmyslania=$OPTARG;;
+        k) declare czasKonsumpcji=$OPTARG;;
         *) echo Nieznana opcja $OPTARG; exit 2;;
     esac
 done
 
          
 #ARGUMENTY DOMYSLNE		 
-if test ${LICZBA_FILOZOFOW:-0} -lt 2; then declare -r LICZBA_FILOZOFOW=5; fi;
-declare -r LICZBA_FILOZOFOW
-if test ${LICZBA_POSILKOW:-0} -lt 2; then declare -r LICZBA_POSILKOW=7; fi;
-declare -r LICZBA_POSILKOW
-declare -r SCIEZKA_KATALOGU=${SCIEZKA_KATALOGU:-stolik}
-declare -r SCIEZKA_BARIERY=".bariera"
-declare SCIEZKA_POTOKU="potok_filozofa"
+if [ ${liczbaFilozofow:-0} -lt 2 ]; then
+	declare -r liczbaFilozofow=5
+fi
+
+declare -r liczbaFilozofow
+
+if [ ${liczbaPosilkow:-0} -lt 2 ]; then
+	declare -r liczbaPosilkow=7
+fi
+
+declare -r liczbaPosilkow
+declare -r sciezkaKatalogu=${sciezkaKatalogu:-stolik}
+declare -r sciezkaBariery=".bariera"
+declare sciezkaPotoku="potok_filozofa"
 
 #SPRAWDZANIE POPRAWNOSCI CZASOW
-if  test -z $CZAS_KONSUMPCJI && test -z $CZAS_ROZMYSLANIA ; then
-	true;
+if [ -z $czasKonsumpcji ] && [ -z $czasRozmyslania ] ; then
+	true
 else
-	czyRzeczywista $CZAS_KONSUMPCJI
-	czyRzeczywista $CZAS_ROZMYSLANIA
+	czyRzeczywista $czasKonsumpcji
+	czyRzeczywista $czasRozmyslania
 fi
 
 
 #STOL
-if [ ! -d $SCIEZKA_KATALOGU ]; then
-	mkdir $SCIEZKA_KATALOGU
+if [ ! -d $sciezkaKatalogu ]; then
+	mkdir $sciezkaKatalogu
 fi
 
 #WIDELCE
-for (( i=1; i<=${LICZBA_FILOZOFOW} ; i++ )); do
-	declare widelec="$SCIEZKA_KATALOGU/$LICZBA_FILOZOFOW"
+for (( i=1; i<=${liczbaFilozofow} ; i++ )); do
+	declare widelec="$sciezkaKatalogu/$liczbaFilozofow"
 	if [ ! -f $widelec ]; then
 		touch $widelec
 	fi
 done
 
 
-declare -r OCZEKIWANA_LICZBA_POLACZEN=$(( LICZBA_FILOZOFOW+1 ))
-export OCZEKIWANA_LICZBA_POLACZEN 
-export SCIEZKA_BARIERY 
-rm -f ${SCIEZKA_BARIERY}
-coproc PODPROCES_bariery { ncat -v -m $OCZEKIWANA_LICZBA_POLACZEN -U -k -l $SCIEZKA_BARIERY -c 'liczba_polaczen=$(netstat -x | grep "$SCIEZKA_BARIERY" | wc -l) ; if [ $liczba_polaczen -eq $OCZEKIWANA_LICZBA_POLACZEN ]; then fuser -k $SCIEZKA_BARIERY; else cat; fi ' ;} 2>&1  
+declare -r oczekiwanaLiczbaPolaczen=$(( liczbaFilozofow+1 ))
+export oczekiwanaLiczbaPolaczen 
+export sciezkaBariery 
+rm -f ${sciezkaBariery}
+coproc podprocesBariery { ncat -v -m $oczekiwanaLiczbaPolaczen -U -k -l $sciezkaBariery -c 'liczbaPolaczen=$(netstat -x | grep "$sciezkaBariery" | wc -l) ; if [ $liczbaPolaczen -eq $oczekiwanaLiczbaPolaczen ]; then fuser -k $sciezkaBariery; else cat; fi ' ;} 2>&1  
 
 # Sprawdzenie czy udalo sie utworzyc proces ncat 
-read odbior <&${PODPROCES_bariery[0]}
-read odbior <&${PODPROCES_bariery[0]}
-if [ "Ncat: Listening on $SCIEZKA_BARIERY" != "$odbior" ]; then
-   echo "Zakonczenie z powodu nieudanej proby otwarcia gniazda $SCIEZKA_BARIERY"
+read odbior <&${podprocesBariery[0]}
+read odbior <&${podprocesBariery[0]}
+if [ "Ncat: Listening on $sciezkaBariery" != "$odbior" ]; then
+   echo "Zakonczenie z powodu nieudanej proby otwarcia gniazda $sciezkaBariery"
    exit 1;
 fi
 
 #URUCHOMIENIE FILOZOFOW
-for i in `shuf -i 1-$LICZBA_FILOZOFOW`; do
+for i in `shuf -i 1-$liczbaFilozofow`; do
 
-	SCIEZKA_TYMCZASOWA=${SCIEZKA_POTOKU}"_"${i}
-    rm -f ${SCIEZKA_TYMCZASOWA};
-    mkfifo ${SCIEZKA_TYMCZASOWA};
+	sciezkaTymczasowa=${sciezkaPotoku}"_"${i}
+    rm -f ${sciezkaTymczasowa};
+    mkfifo ${sciezkaTymczasowa};
 
 	#USTALANIE KOLEJNOSCI DOBIERANIA WIDELCOW
 	if [[ $(($i % 2)) == 1 ]]; then
-		declare PIERWSZY_WIDELEC="$i"
-	    declare DRUGI_WIDELEC="$(($i + 1))"
-	    if [[ $DRUGI_WIDELEC == "$(($LICZBA_FILOZOFOW + 1))" ]]; then DRUGI_WIDELEC=1; fi;
+		declare pierwszyWidelec="$i"
+	    declare drugiWidelec="$(($i + 1))"
+	    if [[ $drugiWidelec == "$(($liczbaFilozofow + 1))" ]]; then
+	    	drugiWidelec=1
+	    fi
     else
-      	declare DRUGI_WIDELEC="$i"
-	    declare PIERWSZY_WIDELEC="$(($i + 1))"
-	    if [[ $PIERWSZY_WIDELEC == "$(($LICZBA_FILOZOFOW + 1))" ]]; then 
-	    	PIERWSZY_WIDELEC=1; fi;
-    fi;   
+      	declare drugiWidelec="$i"
+	    declare pierwszyWidelec="$(($i + 1))"
+	    if [[ $pierwszyWidelec == "$(($liczbaFilozofow + 1))" ]]; then 
+	    	pierwszyWidelec=1
+	    fi
+    fi   
     
     
 	
     #KOMUNIKAT URUCHAMIANIE PROCESU FILOZOFA    
-	if test ${CZAS_ROZMYSLANIA:-losowe} = "losowe"  ;then
-		komunikat "(-)" "<$(hostname)> STARTUJE FILOZOF o FID:$i | Liczba posilkow:$LICZBA_POSILKOW | Liczba filozofow:$LICZBA_FILOZOFOW 
-		| Sciezka katalogu-stolu:$SCIEZKA_KATALOGU | Plik bariery:$SCIEZKA_BARIERY | Plik potoku: $SCIEZKA_TYMCZASOWA | Pierwszy widelec WID:$PIERWSZY_WIDELEC | Drugi widelec WID:$DRUGI_WIDELEC\n"
-	  	filozof $i $LICZBA_POSILKOW $LICZBA_FILOZOFOW $SCIEZKA_KATALOGU $SCIEZKA_BARIERY $PIERWSZY_WIDELEC $DRUGI_WIDELEC $SCIEZKA_TYMCZASOWA &
+	if test ${czasRozmyslania:-losowe} = "losowe"  ;then
+		wiadomosc "(-)" "<$(hostname)> STARTUJE FILOZOF o FID:$i | Liczba posilkow:$liczbaPosilkow | Liczba filozofow:$liczbaFilozofow 
+		| Sciezka katalogu-stolu:$sciezkaKatalogu | Plik bariery:$sciezkaBariery | Plik potoku: $sciezkaTymczasowa | Pierwszy widelec WID:$pierwszyWidelec | Drugi widelec WID:$drugiWidelec\n"
+	  	filozof $i $liczbaPosilkow $liczbaFilozofow $sciezkaKatalogu $sciezkaBariery $pierwszyWidelec $drugiWidelec $sciezkaTymczasowa &
 	else
-		komunikat "(-)" "<$(hostname)> STARTUJE FILOZOF o FID:$i | Liczba posilkow:$LICZBA_POSILKOW | Liczba filozofow:$LICZBA_FILOZOFOW 
-		| Sciezka katalogu-stolu:$SCIEZKA_KATALOGU | Plik bariery:$SCIEZKA_BARIERY | Plik potoku: $SCIEZKA_TYMCZASOWA | Pierwszy widelec WID:$PIERWSZY_WIDELEC | Drugi widelec WID:$DRUGI_WIDELEC | Czas konsumpcji:$CZAS_KONSUMPCJI s | Czas rozmyslania: $CZAS_ROZMYSLANIA s\n"
-		filozof $i $LICZBA_POSILKOW $LICZBA_FILOZOFOW $SCIEZKA_KATALOGU $SCIEZKA_BARIERY $PIERWSZY_WIDELEC $DRUGI_WIDELEC $SCIEZKA_TYMCZASOWA $CZAS_KONSUMPCJI $CZAS_ROZMYSLANIA &
+		wiadomosc "(-)" "<$(hostname)> STARTUJE FILOZOF o FID:$i | Liczba posilkow:$liczbaPosilkow | Liczba filozofow:$liczbaFilozofow 
+		| Sciezka katalogu-stolu:$sciezkaKatalogu | Plik bariery:$sciezkaBariery | Plik potoku: $sciezkaTymczasowa | Pierwszy widelec WID:$pierwszyWidelec | Drugi widelec WID:$drugiWidelec | Czas konsumpcji:$czasKonsumpcji s | Czas rozmyslania: $czasRozmyslania s\n"
+		filozof $i $liczbaPosilkow $liczbaFilozofow $sciezkaKatalogu $sciezkaBariery $pierwszyWidelec $drugiWidelec $sciezkaTymczasowa $czasKonsumpcji $czasRozmyslania &
 	fi
 done
 
 #oczekiwanie na wszystkich filozofow
-komunikat "(-)" "********** Oczekuje na zgloszenie dotarcia do bariery od kazdego filozofa\n"
-ncat -U $SCIEZKA_BARIERY -c 'read'
-komunikat "(-)"  "********** Bariera polowy posilkow zwolniona - Wszyscy filozofowie zjedli polowe posilkow\n"
+wiadomosc "(-)" "********** Oczekuje na zgloszenie dotarcia do bariery od kazdego filozofa\n"
+ncat -U $sciezkaBariery -c 'read'
+wiadomosc "(-)"  "********** Bariera polowy posilkow zwolniona - Wszyscy filozofowie zjedli polowe posilkow\n"
 
 # zwolnienie bariery polowy posilkow
-for i in `shuf -i 1-$LICZBA_FILOZOFOW`; do
-    declare SCIEZKA_POTOKU_FILOZOFA="${SCIEZKA_POTOKU}_$i"
-    echo "kontynuuj" >> ${SCIEZKA_POTOKU_FILOZOFA}
+for i in `shuf -i 1-$liczbaFilozofow`; do
+    declare sciezkaPotokuFilozofa="${sciezkaPotoku}_$i"
+    echo "kontynuuj" >> ${sciezkaPotokuFilozofa}
 done
 # filozofowie kontynuuja uczte
 
 
 wait
 
-czyPosprzatac $SCIEZKA_BARIERY $SCIEZKA_KATALOGU $SCIEZKA_POTOKU $LICZBA_FILOZOFOW
+czyPosprzatac $sciezkaBariery $sciezkaKatalogu $sciezkaPotoku $liczbaFilozofow
 
 exit 0
 
